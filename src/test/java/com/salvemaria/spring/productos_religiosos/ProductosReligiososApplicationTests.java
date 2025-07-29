1c0401dfdf6606c022c1cfee81d061c4711ba3fc
Jotitefrank <100967480+Jotitefrank@users.noreply.github.com>
2025-07-28 19:30:12 -0500

    Inicio de proyecto

---
+++ src/test/java/com/salvemaria/spring/productos_religiosos/ProductosReligiososApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salvemaria.spring.productos_religiosos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosReligiososApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
